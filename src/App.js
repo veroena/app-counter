37390a54ce685df9b12e6420d7a1db89f956a3a1
veroena <veronica.enamorado@gmail.com>
2022-02-21 20:40:13 +0100

    stage 3: change logic and move increase and decrease functions to parent component for access

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const data = [
+const input = [
   { id: 1, value: 0 },
   { id: 2, value: 0 },
   { id: 3, value: 0 },
@@ -8,32 +8,50 @@ const data = [
 ];
 
 function Counter(props) {
-  const { value } = props;
-  const [counter, setCounter] = useState(value);
-
-  const increase = () => {
-    setCounter(value => value + 1)
-  }
-  const decrease = () => {
-    setCounter(value => value - 1)
-  }
+  const { value, id, increase, decrease } = props;
 
   return (
     <div>
-      <b data-testid="value">{counter}</b>
+      <b data-testid="value">{value}</b>
       <div>
-        <button onClick={decrease} data-testid="decrease">-</button>
-        <button onClick={increase} data-testid="increase">+</button>
+        <button onClick={(event) => decrease(event)} data-testid="decrease" id={id}>-</button>
+        <button onClick={(event)=>increase(event)} data-testid="increase"  id={id}>+</button>
       </div>
     </div>
   );
 }
 
 function App() {
+  const [data, setData] = useState(input);
+
+  const increase = (event) => {
+    const id = +event.currentTarget.id;
+    let newData = data.map(item =>
+      {
+        if (item.id === id){
+          return {...item, value: item.value + 1};
+        }
+        return item;
+      });
+    setData(newData);
+  }
+
+  const decrease = (event) => {
+    const id = +event.currentTarget.id;
+    let newData = data.map(item =>
+      {
+        if (item.id === id){
+          return {...item, value: item.value - 1};
+        }
+        return item;
+      });
+    setData(newData);
+  }
+
   return (
     <div>
       {data.map((counter) => (
-        <Counter key={counter.id} value={counter.value} />
+        <Counter key={counter.id} value={counter.value} id={counter.id} increase={increase} decrease={decrease} />
       ))}
     </div>
   );
